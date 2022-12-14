be492b24d7608d5be5501b7b4ca63d309eec4f4c
Naren0325 <pnarendran@gmail.com>
2022-12-14 20:27:44 +0530

    mgs

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routerfile from './components/routerfile';
-
-
-
-
+//naren
 function App() {
   return (
     <div className="App">
