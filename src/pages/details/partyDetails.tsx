309db47eb6047589427ea67e7e1672ed21f3ab92
Naren0325 <pnarendran@gmail.com>
2022-12-20 19:08:46 +0530

    enquriy

--- src/pages/details/partyDetails.tsx
+++ src/pages/details/partyDetails.tsx
@@ -1,65 +1,82 @@
-import React from "react";
-import ImageGallery from 'react-image-gallery';
-import PH1 from '../../assets/images/Party-Hall/party1.jpeg'
-import PH2 from '../../assets/images/Party-Hall/party2.jpeg'
-import PH3 from '../../assets/images/Party-Hall/party3.jpeg'
-import PH4 from '../../assets/images/Party-Hall/party4.jpeg'
-import PH5 from '../../assets/images/Party-Hall/party5.jpeg'
-import PH6 from '../../assets/images/Party-Hall/party6.jpeg'
-import PH7 from '../../assets/images/Party-Hall/party7.jpeg'
-import PH8 from '../../assets/images/Party-Hall/party8.jpeg'
-import PH9 from '../../assets/images/Party-Hall/party2.jpeg'
-import PH10 from '../../assets/images/Party-Hall/party1.jpeg'
+import React, { useState } from "react";
+import party1 from '../../assets/images/Party-Hall/party1.jpeg'
+import party2 from '../../assets/images/Party-Hall/party2.jpeg'
+import party3 from '../../assets/images/Party-Hall/party3.jpeg'
+import party4 from '../../assets/images/Party-Hall/party4.jpeg'
+import party5 from '../../assets/images/Party-Hall/party5.jpeg'
+import party6 from '../../assets/images/Party-Hall/party6.jpeg'
+import party7 from '../../assets/images/Party-Hall/party7.jpeg'
+import party8 from '../../assets/images/Party-Hall/party8.jpeg'
+import party9 from '../../assets/images/Party-Hall/party2.jpeg'
+import party10 from '../../assets/images/Party-Hall/party1.jpeg'
+import { Container, Modal, Row } from "react-bootstrap";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/esm/Col";
+import ImgGallery from "../../components/imgGallery/imgGallery";
+import ImagePreview from "../../components/imgPreview/imagePreview";
 
 
 
 const images = [
-    {
-        original:PH1 ,
-        thumbnail: PH1,
-    },
-    {
-        original:PH2,
-        thumbnail:PH2,
-    },
-    {
-        original:PH3,
-        thumbnail:PH3,
-    },
-    {
-        original:PH4,
-        thumbnail:PH4,
-    },
-    {
-        original:PH5,
-        thumbnail:PH5,
-    },
-    {
-        original:PH6,
-        thumbnail:PH6,
-    },
-    {
-        original:PH7,
-        thumbnail:PH7,
-    },
-    {
-        original:PH8,
-        thumbnail:PH8,
-    },
-    {
-        original:PH9,
-        thumbnail:PH9,
-    },
-    {
-        original:PH10,
-        thumbnail:PH10,
-    },
+    party1,
+    party2,
+    party3,
+    party4,
+    party5,
+    party6,
+    party7,
+    party8,
+    party9,
+    party10,
 ]
 
-export default function PartyDetails(){
+export default function PartyDetails() {
+    const [show, setShow] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+  
     return (
-        <div><h3>Our Beautiful Partyhall view images</h3>
-         <div><ImageGallery items={images} /> </div>
-         </div>
-    )
-}
+      <Container>
+        <Header />
+        <Container>
+          <div className="row mt-5">
+            <div className="col-lg-4">
+              <Image
+                className="rounded-circle"
+                width="140"
+                height="140"
+                src={party1}
+              ></Image>
+              <h2>Party Hall</h2>
+              <p>
+              A Party hall, function hall, or reception hall, is a special purpose room, or a building, used for hosting large social and business events.
+              </p>
+            </div>
+          </div>
+          <Row>
+            <Col>
+              <ImgGallery
+                data={images}
+                onItemClick={(index: any) => {
+                  /// alert("Index .... " + index);
+                  setSelectedIndex(index);
+                  setShow(true);
+                }}
+              />
+            </Col>
+          </Row>
+  
+          <ImagePreview
+            data={images}
+            selectedItemIndex={selectedIndex}
+            show={show}
+            setShow={setShow}
+          />
+        </Container>
+  
+        <Footer />
+      </Container>
+    );
+  }
+  
--- src/pages/enquriypage.tsx
+++ src/pages/enquriypage.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import Header from "../components/header/header";
+import NoTransitionExample from "./homepage/carcoual";
+import Footer from "../components/footer/footer";
 
 export default function EnquriyPage() {
   return (
-    <div>enquriy</div>
-  )
+    <>
+      <Header />
+      <NoTransitionExample />
+      <div>
+        <table width={"400"}>
+          <tr>
+            <td height={"30"}>Name</td>
+            <td>
+              <input type="text" />
+            </td>
+          </tr>
+          <tr>
+            <td height={"30"}>Email</td>
+            <td>
+              <input type="email" />
+            </td>
+          </tr>
+          <tr>
+            <td height={"30"}>Mobile</td>
+            <td>
+              <input type="Moble" />
+            </td>
+          </tr>
+          <tr>
+            <td>Enquiry Type</td>
+            <td>
+              <select>
+                <option>Room</option>
+                <option>Conference Hall</option>
+                <option>Party Hall</option>
+                <option>Business Meeting</option>
+                <option>Birthday Hall</option>
+                <option>Other</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <td height={"30"}>Commends</td>
+              <td>
+                <textarea name="message"></textarea>
+              </td>
+            </td>
+          </tr>
+          <tr>
+            <td height={"50"} align="center"></td>
+            <td>
+              <input type="button" value={"Submite"} />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <Footer />
+    </>
+  );
 }
