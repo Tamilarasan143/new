0e8090af9dd16338cdcaca29d0c9ca21e0cf2b58
Jegadeesan Muthusamy <jega@lirisoft.com>
2023-01-02 16:06:30 +0530

    Fixed Full height issue

--- src/pages/my-account/header.tsx
+++ src/pages/my-account/header.tsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function MyAccountHeader() {
   return (
     <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/acc">
+      <a
+        className="navbar-brand col-auto col-md-2 col-xl-2 px-sm-2 px-3"
+        href="/acc"
+      >
         Mithra Resorts & Club
       </a>
       <button
--- src/pages/my-account/layout.tsx
+++ src/pages/my-account/layout.tsx
@@ -1,54 +1,53 @@
-import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router";
 import React from "react";
 import "./layout.css";
 import MyAccountHeader from "./header";
 import MySideBar from "./sidebar";
 
-
 export default function MyAccount() {
   return (
     <>
-      <MyAccountHeader />
-      <div className="container-fluid">
-        <div className="row">
-          <MySideBar />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 className="h2">Dashboard</h1>
-              <div className="btn-toolbar mb-2 mb-md-0">
-                <div className="btn-group me-2">
+      <div className="container-fluid vh-100">
+        <div className="row flex-nowrap">
+          <MyAccountHeader />
+        </div>
+        <div className="row flex-nowrap vh-100">
+          <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
+            <MySideBar />
+          </div>
+          <div className="col py-3">
+            <main className="container-fluid vh-100">
+              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Dashboard</h1>
+                <div className="btn-toolbar mb-2 mb-md-0">
+                  <div className="btn-group me-2">
+                    <button
+                      type="button"
+                      className="btn btn-sm btn-outline-secondary"
+                    >
+                      Share
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-sm btn-outline-secondary"
+                    >
+                      Export
+                    </button>
+                  </div>
                   <button
                     type="button"
-                    className="btn btn-sm btn-outline-secondary"
+                    className="btn btn-sm btn-outline-secondary dropdown-toggle"
                   >
-                    Share
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                  >
-                    Export
+                    <span data-feather="calendar"></span>
+                    This week
                   </button>
                 </div>
-                <button
-                  type="button"
-                  className="btn btn-sm btn-outline-secondary dropdown-toggle"
-                >
-                  <span data-feather="calendar"></span>
-                  This week
-                </button>
               </div>
-            </div>
-            <Outlet />
-          </main>
+              <Outlet />
+            </main>
+          </div>
         </div>
       </div>
-      <footer className="footer mt-auto py-3 bg-light">
-        <div className="container">
-          <span className="text-muted">Place sticky footer content here.</span>
-        </div>
-      </footer>
     </>
   );
 }
